a4bf576620249b6998a21fba37a2434237554945
phucloi97 <phucloi97@hotmail.com>
2021-01-31 15:17:26 +0700

    add strategy jwt

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule,
+    JwtModule.register({
+      secret: 'deptraivl',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { userLogin } from './dto/user-login.dto';
 import { UserDto } from './dto/user.dto';
@@ -7,17 +8,22 @@ import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) private userRepository: UserRepository) {}
+  constructor(
+    @InjectRepository(User) private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
   async createUser(userDto: UserDto) {
     return this.userRepository.createUser(userDto);
   }
   async signIn(userLogin: userLogin) {
     const { email, password } = userLogin;
-    const user = await this.userRepository.findOne({ email });
+    let user = await this.userRepository.findOne({ email });
     let match = await user.validatePassword(password);
     console.log(user);
     if (!match) {
       throw new UnauthorizedException();
-    } else return 'login ok';
+    }
+    const payload = { user_name: user.user_name, id: user.id };
+    return this.jwtService.sign(payload);
   }
 }
